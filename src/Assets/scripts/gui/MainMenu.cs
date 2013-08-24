143de0f5eabbc63698c1f92a2c31dec0b94a01f9
Borislav Kosharov <bosakmaw@gmail.com>
2013-08-24 22:00:18 +0300

    Created main menu

--- src/Assets/scripts/gui/MainMenu.cs
+++ src/Assets/scripts/gui/MainMenu.cs
@@ -1,15 +1,61 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 
 public class MainMenu : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
+	public enum MenuState {
+		Main,
+		Credits,
+		Play
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	public GUISkin Skin;
+	public MenuState State;
 	
+	private Rect screen;
+	
+	void Start () {
+		screen = new Rect(0, 0, Screen.width, Screen.height);
+	}
+	
+	void OnGUI() {
+		GUI.skin = Skin;
+		GUILayout.BeginArea(screen);
+			GUILayout.BeginVertical();
+			switch(State) {
+				case MenuState.Main:
+					GUILayout.FlexibleSpace();
+					MenuButton("Play", Color.green, () => State = MenuState.Play);
+					MenuButton("Credits", Color.blue, () => State = MenuState.Credits);
+					MenuButton("Quit", Color.red, Application.Quit);
+					GUILayout.FlexibleSpace();
+					break;
+				case MenuState.Credits:
+					GUILayout.FlexibleSpace();
+					MenuButton("Borislav Kosharov", Color.yellow);
+					MenuButton("Viktor Danev", Color.cyan);
+					MenuButton("Back", Color.magenta, () => State = MenuState.Main);
+					GUILayout.FlexibleSpace();
+					break;
+				case MenuState.Play:
+					GUILayout.FlexibleSpace();
+					MenuButton("Random", Color.green, () => Application.LoadLevel("Game"));
+					MenuButton("Back", Color.magenta, () => State = MenuState.Main);
+					GUILayout.FlexibleSpace();
+					break;
+			}
+			GUILayout.EndVertical();
+		GUILayout.EndArea();
+	}
+	
+	private void MenuButton(string text, Color color, Action action = null) {
+		Color temp = GUI.contentColor;
+		GUILayout.BeginHorizontal();
+			GUILayout.FlexibleSpace();
+			GUI.contentColor = color;
+			if(GUILayout.Button(text) && action != null)
+				action();
+		GUILayout.EndHorizontal();
+		GUI.contentColor = temp;
 	}
 }
