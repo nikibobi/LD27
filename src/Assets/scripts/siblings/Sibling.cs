954ae031a8fbb41c66c567581539e89d06d9116b
Borislav Kosharov <bosakmaw@gmail.com>
2013-08-24 12:01:18 +0300

    Added basic project structure
    Main scene and Sibling beginning

---
+++ src/Assets/scripts/siblings/Sibling.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using Spine;
+
+public abstract class Sibling : MonoBehaviour {
+	
+	public float Health { get; set; }
+	public bool Ability { get; set; }
+	public Skin Skin { get; protected set; }
+	
+	void Start() {
+		Health = 100f;
+		Ability = true;
+	}
+	
+	void Update() {
+		
+	}
+	
+	public bool UseAbility() {
+		if(!Ability)
+			return false;
+		MyAbility();
+		return Ability = false;
+	}
+	
+	protected abstract void MyAbility();
+	
+	public bool Attack() {
+		//attack and return true if the attack was successfull?
+		throw new NotImplementedException("attack not implemented");
+	}
+	
+	public void Move(Vector2 direction) {
+		//move the character in that direction
+	}
+}
